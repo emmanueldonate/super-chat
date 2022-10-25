3fa74671f5031f502ba843e10ba54a14f8e67d5d
unknown <e.o.donate@gmail.com>
2022-10-25 15:15:10 -0700

    Update

---
+++ components/component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+       
+      </header>
+      <section>
+      
+      </section>
+    </div>
+  );
+}
+
+
+export default App;
