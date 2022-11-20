fcb0d98deb773ef697cdab83d05118acd9b606e8
Emmanuel Donate <e.o.donate@gmail.com>
2022-11-19 21:00:20 -0800

    Update component.js

--- components/component.js
+++ components/component.js
@@ -4,12 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
-       
-      </header>
-      <section>
-      
-      </section>
+     
+ 
     </div>
   );
 }
